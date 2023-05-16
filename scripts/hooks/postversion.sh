14571973faae8116c4800a6f72682eb3d90f5777
Alex Hicks <alex@alexhicks.net>
2023-05-16 01:16:42 -0400

    Implement scripts/hooks/postversion.sh

---
+++ scripts/hooks/postversion.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+tag_prefix="$1"
+if [ "$tag_prefix" == "" ]; then
+  >&2 echo "Usage: scripts/hooks/postversion.sh <tag-prefix>"
+  exit 1
+fi
+
+version=$(cat package.json | jq '.version')
+tag="$tag_prefix-v$version"
+commit_message="$tag_prefix@$version"
+
+echo "Committing new git tag: $tag at commit '$commit_message'"
+git add package.json
+git commit -m "$commit_message"
+git tag $tag
+
+git push
+git push --tags
