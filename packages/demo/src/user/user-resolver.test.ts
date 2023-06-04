735a17831c0b68a7501916cbebb70ba9976cf586
Alex Hicks <alex@alexhicks.net>
2023-06-04 10:32:15 -0400

    Add demo test script

---
+++ packages/demo/src/user/user-resolver.test.ts
@@ -0,0 +1,23 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+import { UserResolver } from "./user-resolver.js";
+
+describe("user-resolver", () => {
+  const userResolver = new UserResolver();
+  describe("#users", async () => {
+    it("should return some users with usernames", async () => {
+      const expected = [{ username: "foo" }, { username: "bar" }];
+      const actual = await userResolver.users();
+      assert.deepStrictEqual(actual, expected);
+    });
+  });
+  describe("#id", () => {
+    it("should return md5 hashes of usernames", async () => {
+      // md5 of my name. this is a demo
+      const expected = ["534b44a19bf18d20b71ecc4eb77c572f"];
+      const actual = await userResolver.id([{ username: "alex" }]);
+      assert.deepStrictEqual(actual, expected);
+    });
+  });
+});
--- packages/demo/src/user/user-resolver.ts
+++ packages/demo/src/user/user-resolver.ts
@@ -11,7 +11,7 @@ export class UserResolver {
   }
 
   @resolveField("User.id", true)
-  async id(users: IUser[]): Promise<IUser["id"][]> {
+  async id(users: Omit<IUser, "id">[]): Promise<IUser["id"][]> {
     return users.map((u) =>
       crypto.createHash("md5").update(u.username).digest("hex")
     );
