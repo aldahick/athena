588f111296c0d1e7b75c05222e5fc6202df48223
Alex Hicks <alex@alexhicks.net>
2023-06-04 23:58:42 -0400

    Update demo for new config decorator

--- packages/demo/src/config.ts
+++ packages/demo/src/config.ts
@@ -1,10 +1,14 @@
 import { resolve } from "node:path";
 
-import { BaseConfig } from "@athenajs/core";
+import { BaseConfig, config } from "@athenajs/core";
 import { getModuleDir } from "@athenajs/utils";
 
+@config()
 export class Config extends BaseConfig {
   readonly graphqlSchemaDirs = [
     resolve(getModuleDir(import.meta), "../schema"),
   ];
+  http = {
+    port: Number(this.required("HTTP_PORT")),
+  };
 }
--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -1,18 +1,13 @@
 import "reflect-metadata";
+import "./config.js";
 import "./hello/index.js";
 import "./user/index.js";
 
-import { Application, BaseConfig, container, Logger } from "@athenajs/core";
+import { Application, createApp } from "@athenajs/core";
 import { isModuleMain } from "@athenajs/utils";
 
-import { Config } from "./config.js";
-
 export const main = async (): Promise<Application> => {
-  container.registerInstance(Logger, new Logger({}));
-  const config = new Config();
-  container.registerInstance(Config, config);
-  container.registerSingleton(BaseConfig, Config);
-  const app = container.resolve(Application);
+  const app = createApp();
   await app.start();
   return app;
 };
