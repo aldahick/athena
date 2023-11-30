a5a6390bbd11711e17356744f5539e9e80870f15
Alex Hicks <alex@alexhicks.net>
2023-11-29 22:20:16 -0500

    Add LoggerOptions.pretty and tests for Logger

---
+++ packages/core/src/logger.test.ts
@@ -0,0 +1,87 @@
+import "reflect-metadata";
+
+import assert from "node:assert";
+import { after, afterEach, before, describe, it, Mock, mock } from "node:test";
+
+import { BaseConfig } from "./config.js";
+import { Logger } from "./logger.js";
+
+describe("logger", () => {
+  let logMock: Mock<NodeJS.WriteStream["write"]>;
+  before(() => {
+    logMock = mock.method(process.stdout, "write");
+    logMock.mock.mockImplementation(() => {});
+  });
+  afterEach(() => {
+    logMock.mock.resetCalls();
+  });
+  after(() => {
+    logMock.mock.restore();
+  });
+
+  it("should print debug messages when options.level is debug", () => {
+    class Config extends BaseConfig {
+      graphqlSchemaDirs = [];
+      http = { port: -1 };
+      log = {
+        level: "debug",
+      };
+    }
+
+    const logger = new Logger(new Config());
+    logger.debug("test");
+    const firstCall = logMock.mock.calls[0];
+    assert.ok(firstCall);
+    assert.deepStrictEqual(firstCall.arguments, [
+      JSON.stringify({ level: "debug", message: "test" }) + "\n",
+    ]);
+  });
+
+  it("should not print debug messages when options.level is info", () => {
+    class Config extends BaseConfig {
+      graphqlSchemaDirs = [];
+      http = { port: -1 };
+      log = {
+        level: "info",
+      };
+    }
+
+    const logger = new Logger(new Config());
+    logger.debug("test");
+    assert.strictEqual(logMock.mock.callCount(), 0);
+  });
+
+  it("should print JSON when options.pretty is false", () => {
+    class Config extends BaseConfig {
+      graphqlSchemaDirs = [];
+      http = { port: -1 };
+      log = {
+        pretty: false,
+      };
+    }
+
+    const logger = new Logger(new Config());
+    logger.info("test");
+    const firstCall = logMock.mock.calls[0];
+    assert.ok(firstCall);
+    assert.deepStrictEqual(firstCall.arguments, [
+      JSON.stringify({ level: "info", message: "test" }) + "\n",
+    ]);
+  });
+
+  it("should print beautifully when options.pretty is true", () => {
+    class Config extends BaseConfig {
+      graphqlSchemaDirs = [];
+      http = { port: -1 };
+      log = {
+        pretty: true,
+      };
+    }
+
+    const logger = new Logger(new Config());
+    logger.info("test");
+    const firstCall = logMock.mock.calls[0];
+    assert.ok(firstCall);
+    assert.deepStrictEqual(firstCall.arguments, ["info: test\n"]);
+  });
+});
--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -5,13 +5,20 @@ import { BaseConfig, injectConfig } from "./config.js";
 
 const WinstonLogger = winston.createLogger as unknown as typeof winston.Logger;
 
-export type LoggerOptions = winston.LoggerOptions;
+export type LoggerOptions = winston.LoggerOptions & {
+  pretty?: boolean;
+};
 
 @injectable()
 export class Logger extends WinstonLogger {
   constructor(@injectConfig() config: BaseConfig) {
     super({
       transports: [new winston.transports.Console()],
+      ...(config.log?.pretty
+        ? {
+            format: winston.format.simple(),
+          }
+        : {}),
       ...config.log,
     });
   }
