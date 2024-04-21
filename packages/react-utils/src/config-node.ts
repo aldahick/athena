cd6eb83177d371c5a5c78d51515517f1b7b155b6
Alex Hicks <alex@alexhicks.net>
2024-04-21 14:13:47 -0400

    Remove console.log

--- packages/react-utils/src/config-node.ts
+++ packages/react-utils/src/config-node.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath } from "node:url";
 import { addHtmlConfigAttributes } from "./config.js";
 
-console.log("test", process.argv);
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   await addHtmlConfigAttributes(...process.argv.slice(2));
 }
