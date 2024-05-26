edacb4571503dcca59ca410266564008fb41b3e6
Alex Hicks <alex@alexhicks.net>
2024-05-26 10:43:47 -0400

    Export injectAll from core index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,12 @@
 import "reflect-metadata";
 export * from "./application.js";
 export * from "./config.js";
-export { container, injectable, makeRegistryDecorator } from "./container.js";
+export {
+  container,
+  injectable,
+  injectAll,
+  makeRegistryDecorator,
+} from "./container.js";
 export * from "./graphql/index.js";
 export * from "./http/index.js";
 export * from "./logger.js";
